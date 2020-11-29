96b23ab98d32186d4980c1548862b5b8be1556bd
sarasandager <sara590x@edu.easj.dk>
2020-11-29 21:36:48 +0100

    Added isEmpty() on the textFields and an error message if true

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -10,15 +11,21 @@ public class Controller {
     TextField textField1;
     public TextField inputA;
     public TextField inputB;
+    public Label errorMessage;
 
     public void showResult(){
+        if (inputA.getText() == null || inputA.getText().trim().isEmpty() || inputB.getText() == null || inputB.getText().trim().isEmpty()) {
+            errorMessage.setStyle("-fx-opacity: 1");
+        } else{
+            errorMessage.setStyle("-fx-opacity: 0");
 
-        int number1 = Integer.parseInt(inputA.getText());
-        int number2 = Integer.parseInt(inputB.getText());
-        int sum = number1+number2;
+            int number1 = Integer.parseInt(inputA.getText());
+            int number2 = Integer.parseInt(inputB.getText());
 
-        textField1.clear();
-        System.out.println("Button calculate was clicked");
-        textField1.appendText(""+sum);
+            int sum = number1+number2;
+            textField1.clear();
+            System.out.println("Button calculate was clicked");
+            textField1.appendText(String.valueOf(sum));
+        }
     }
 }
